cecb20c663ab54d920fb96258ad44f6f5fce9e9c
divya turuballi <divyanaidu418@gmail.com>
2025-02-06 19:38:55 +0530

    Initial project setup

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testOutput() {
+        String message = Hello, Jenkins!;
+        assertEquals(message, Hello, Jenkins!);
+    }
+}
+
